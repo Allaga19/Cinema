d40b385b37b9dbaa3f3bc0e13823e209fe5ae236
Алла Гаврюшина <https://github.com/Allaga19>
2021-10-18 16:12:05 +0300

    Адаптив и анимация

--- js/script.js
+++ js/script.js
@@ -1,28 +1,26 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector('.price-total');
-//стоимость билета
+const menuButton =document.querySelector('.m-menu');
+const menu =document.querySelector('.menu');
+
+// стоимость билета
 let cost = 800;
 // общая сумму: цена + колличество
 let totalPrice = 0;
 schemeSvg.addEventListener('click', (event) => {
-   // console.log("клик посхеме ");
-   // проверка куда кликнул
-   // console.log(event.target);
-   //находим элемент (event.target) на который кликнули и вешаем на него обработчик события
-   // event.target.classList.toggle('active');
    // проверка есть ли у элемента класс booked
    if (!event.target.classList.contains('booked')) {
-      // если есть, то
-      // console.log('ops no');
       // если место незанято, вешаем класс active 
       event.target.classList.toggle('active');
-      // totalPrice = schemeSvg.querySelectorAll('.active').length * cost;
-      // запись по другому
       let totalSeats = schemeSvg.querySelectorAll('.active').length;
       totalPrice = totalSeats * cost;
       totalPriceTag.textContent = totalPrice;
-      // находим все элементы(кресла), которые выбраны( количество)
-      // console.log(schemeSvg.querySelectorAll('.active').length);
    }
    
+});
+// menu
+menuButton.addEventListener('click', () => {
+   console.log('click');
+   // menu.style.display = "block";
+   menu.classList.toggle("is-open");
 });
